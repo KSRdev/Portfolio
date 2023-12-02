6484ed55d627e9a425ec019d061ac11ec49092ff
KSRdev <simoneradaelli@yahoo.com>
2023-12-02 13:04:18 +0100

    Version 2.0.1

--- src/app/components/hero.tsx
+++ src/app/components/hero.tsx
@@ -6,7 +6,7 @@ import "../css/hero.css";
 
 export default function Hero() {
   return (
-    <section id="Hero" className="h-screen flex flex-col justify-center items-center ">
+    <section id="Hero" className="h-screen flex flex-col justify-center items-center mt--2 ">
       <div className="ProfileImage grid justify-items-center">
         <div className="profile__border ">
         </div>
